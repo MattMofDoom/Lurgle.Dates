51637e867d3f7b82369fa67f69e5ba34a53bd557
MattMofDoom <30714560+MattMofDoom@users.noreply.github.com>
2021-09-17 14:00:00 +1000

    De-duplicate day of week and month of year parsing Allow for short day names to be passed to day of week parsing

---
+++ Lurgle.Dates/Enums/ShortDayOfWeek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lurgle.Dates
+{
+    /// <summary>
+    /// Alternative DayOfWeek enum for short day names
+    /// </summary>
+    public enum ShortDayOfWeek
+    {
+        /// <summary>
+        /// Sunday
+        /// </summary>
+        Sun = 0,
+        /// <summary>
+        /// Monday
+        /// </summary>
+        Mon = 1,
+        /// <summary>
+        /// Tuesday
+        /// </summary>
+        Tue = 2,
+        /// <summary>
+        /// Wednesday
+        /// </summary>
+        Wed = 3,
+        /// <summary>
+        /// Thursday
+        /// </summary>
+        Thu = 4,
+        /// <summary>
+        /// Friday
+        /// </summary>
+        Fri = 5,
+        /// <summary>
+        /// Saturday
+        /// </summary>
+        Sat = 6
+    }
+}
